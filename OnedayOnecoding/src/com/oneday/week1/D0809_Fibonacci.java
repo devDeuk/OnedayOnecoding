3dbec2332150c3cdb9577032db395721afa6d4aa
d e u k <d e u k@SKT-INT8-A093.sktsoc.ad>
2019-08-08 14:46:56 +0900

    8월8일

---
+++ OnedayOnecoding/src/com/oneday/week1/D0809_Fibonacci.java
@@ -0,0 +1,63 @@
+package com.oneday.week1;
+
+import java.util.Scanner;
+
+/**
+ * @FileName : Fibonacci.java
+ * @Project : OnedayOnecoding
+ * @Date : 2019. 8. 8. 
+ * @작성자 : d e u k
+ * @변경이력 :
+ * @프로그램 설명 : 
+ * 피보나치 배열은 0과 1로 시작하며, 다음 피보나치 수는 바로 앞의 두 피보나치의 수의 합이 된다. 
+ * 정수 N이 주어지면, N보다 작은 모든 짝수 피보나치 수의 합을 구하여라
+ * 예제)
+ * Input 
+ *  -  N = 12
+ * Output
+ *  - 10 ( 0,1,2,3,5,8 중 짝수인 2+8= 10) 
+*/
+public class D0809_Fibonacci {
+
+	/*
+	 1. input 값을 받는다. 
+	 2. input 받은 값에 피보나치 배열을 만든다.
+	 3. 피보나치배열에서 짝수를 추출한다.
+	 4. 추출한 짝을수를 더한다.
+	 5. 결과를 노출한다.
+	 */
+
+
+	
+	public static void main(String[] args) throws Exception{
+
+		
+		//1.input 값받기 
+		System.out.print("숫자를 입력해주세요 : ");
+		Scanner input1 = new Scanner(System.in);
+		int inpNum = input1.nextInt();
+		
+		
+		System.out.println("피보나츠 배열을 생성중입니다.");
+		Thread.sleep(1000);
+		
+
+		
+		
+		
+		
+		System.out.println("피보나츠 짝수를 추출중입니다.");
+		Thread.sleep(1000);
+		
+		System.out.println("피보나츠 짝수에서 추출한 합계를 계산중입니다.");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	
+}
